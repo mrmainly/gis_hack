e40ef042985ec8ee4a039910551ba1ad4c9f76ba
Gavril Rebrov <gavril.rebrov@gmail.com>
2021-03-20 13:54:51 +0900

    map component

--- src/pages/MainPageYoung/mainPageYong.js
+++ src/pages/MainPageYoung/mainPageYong.js
@@ -2,11 +2,15 @@ import React from 'react'
 import Layout from '../../components/layout/Layout'
 import NewMangaList from './components/NewMangaList'
 
+import MapComponent from '../../components/MapComponent'
+
 const MainPageYoung = () => {
 
     return (
         <Layout>
             <NewMangaList title={"Новые главы манги"} />
+
+            <MapComponent />
         </Layout>
     )
 }
