054879e9cb082a167586bfc7e4f1e94998211913
Gavril Rebrov <gavril.rebrov@gmail.com>
2021-03-20 14:27:40 +0900

    add map component

--- src/pages/MainPageYoung/mainPageYong.js
+++ src/pages/MainPageYoung/mainPageYong.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Layout from '../../components/layout/Layout'
 import FirstComponent from './components/FurstComponent'
+import MapComponent from '../../components/MapComponent'
 
 const MainPageYoung = () => {
 
     return (
         <Layout>
+            <MapComponent />
+
             <FirstComponent />
         </Layout>
     )
